1d9c8622c11f60754bebf839a25cb71225b04406
David Reichardt <davereichardt@gmail.com>
2025-03-23 12:39:10 -0400

    ability to draw on grid with mouseover

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 const sizeBtn = document.querySelector('.grid-size-prompt');
 const gridContainer = document.querySelector('.grid-container');
-// let defaultrows = 16;
+let childElement = gridContainer.lastElementChild;
 let size = 0;
 
 sizeBtn.addEventListener('click', getGridSize);
 
-function removeExistingGrid() {
-    let childElement = gridContainer.lastElementChild;
+function draw() {
+  for (let i = 1; i <= gridContainer.children.length; i++) {
+    let box = document.getElementById(`box-${i}`);
+    
+    box.addEventListener('mouseover', function() {
+      box.style.background = 'black';
+    });
+  }
+}
 
-    while (childElement) {
-      gridContainer.removeChild(childElement);
-      childElement = gridContainer.lastElementChild; 
-    }
+function removeExistingGrid() {
+  while (childElement) {
+    gridContainer.removeChild(childElement);
+    childElement = gridContainer.lastElementChild;
+  }
 }
 
 function getGridSize(numberOfColumns) {
   numberOfColumns = window.prompt('Enter size of grid (Max 100)');
-  
+
   removeExistingGrid();
 
   if (numberOfColumns <= 100 && numberOfColumns >= 2) {
     size = numberOfColumns * numberOfColumns; // enough boxes to equal even rows and columns
     generateGrid(size, numberOfColumns);
+    draw();
   } else {
     window.alert('Please enter a number greater than 1 and less than 100');
     getGridSize(numberOfColumns);
@@ -36,10 +45,9 @@ function generateGrid(size, numberOfColumns) {
     const newBox = document.createElement('div');
 
     newBox.className = 'grid-box';
+    newBox.id = `box-${i}`;
     newBox.style.width = `${divSize}px`;
     newBox.style.height = `${divSize}px`;
     gridContainer.appendChild(newBox);
   }
 }
-
-//calculate width and height based on how many rows compared to how wide container is
